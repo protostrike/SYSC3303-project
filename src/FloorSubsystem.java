327695d1d067d6e1db5f8ce0cb87d0f976d19c99
aryan <Aryan.r.t@hotmail.com>
2019-01-27 14:37:36 -0500

    Update FloorSubsystem.java

--- src/FloorSubsystem.java
+++ src/FloorSubsystem.java
@@ -1,4 +1,123 @@
+// SimpleEchoClient.java
+// This class is the client side for a simple echo server based on
+// UDP/IP. The client sends a character string to the echo server, then waits 
+// for the server to send it back to the client.
+// Last edited January 9th, 2016
 
-public class FloorSubsystem {
+import java.awt.List;
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
 
+public class FloorSubsystem{
+
+   DatagramPacket sendPacket, receivePacket;
+   DatagramSocket sendReceiveSocket;
+
+   
+   private byte[] convertToBytes(Object object) throws IOException {
+	    try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	         ObjectOutput out = new ObjectOutputStream(bos)) {
+	        out.writeObject(object);
+	        return bos.toByteArray();
+	    } 
+	}
+
+   public FloorSubsystem()
+   {
+      try {
+
+         sendReceiveSocket = new DatagramSocket();
+      } catch (SocketException se) {   // Can't create the socket.
+         se.printStackTrace();
+         System.exit(1);
+      }
+   }
+
+   public void sendAndReceive()
+   {
+      // Prepare a DatagramPacket and send it via sendReceiveSocket
+      // to port 5000 on the destination host.
+ 
+	   byte msg[] = null; 
+     
+      Person person = new Person("14:05:15.0", 1, 10, true);
+     
+     
+      System.out.println(person);
+      try {
+		msg = this.convertToBytes(person);
+      } catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+      }
+      
+      
+     
+
+
+      //  5000 - the destination port number on the destination host.
+      try {
+         sendPacket = new DatagramPacket(msg, msg.length,
+                                         InetAddress.getLocalHost(), 5000);
+      } catch (UnknownHostException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      System.out.println("Client: Sending packet:");
+      System.out.println("To host: " + sendPacket.getAddress());
+      System.out.println("Destination host port: " + sendPacket.getPort());
+      int len = sendPacket.getLength();
+      System.out.println("Length: " + len);
+      System.out.print("Containing: ");
+      System.out.println(new String(sendPacket.getData(),0,len)); // or could print "s"
+
+      // Send the datagram packet to the server via the send/receive socket. 
+
+      try {
+         sendReceiveSocket.send(sendPacket);
+      } catch (IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      System.out.println("Client: Packet sent.\n");
+      /*
+      // Construct a DatagramPacket for receiving packets up 
+      // to 100 bytes long (the length of the byte array).
+
+      byte data[] = new byte[100];
+      receivePacket = new DatagramPacket(data, data.length);
+
+      try {
+         // Block until a datagram is received via sendReceiveSocket.  
+         sendReceiveSocket.receive(receivePacket);
+      } catch(IOException e) {
+         e.printStackTrace();
+         System.exit(1);
+      }
+
+      // Process the received datagram.
+      System.out.println("Client: Packet received:");
+      System.out.println("From host: " + receivePacket.getAddress());
+      System.out.println("Host port: " + receivePacket.getPort());
+      len = receivePacket.getLength();
+      System.out.println("Length: " + len);
+      System.out.print("Containing: ");
+
+      // Form a String from the byte array.
+      String received = new String(data,0,len);   
+      System.out.println(received);
+
+      // We're finished, so close the socket.
+      sendReceiveSocket.close();
+      */
+   }
+
+   public static void main(String args[])
+   {
+      FloorSubsystem c = new FloorSubsystem();
+      c.sendAndReceive();
+   }
 }
